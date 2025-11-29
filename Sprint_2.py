ec843c5003e5f317419bfd22de9f926d59c01fc3
Virginia Lee <vilee2@terpmail.umd.edu>
2025-11-29 00:02:55 -0800

    moving code to jupyter notebook

--- Sprint_2.py
+++ Sprint_2.py
@@ -1,13 +1,26 @@
 # INST414 Final Project Sprint 2
 
+## Importing necessary libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import chi2_contingency
 
-cols_to_keep = ['Data_Value', 'Race/Ethnicity', 'Age(months)', 'Sex', 'LocationDesc']
+# select columns to read and keep
+cols_to_keep = ['Data_Value', 'Race/Ethnicity', 'Age(months)', 'Sex', 'LocationDesc', 'LocationAbbr', 'YearStart', 'YearEnd', 'Low_Confidence_Limit', 'High_Confidence_Limit', 'Sample_Size']
 df = pd.read_csv("/Users/virginialee/Downloads/WIC_data.csv", usecols=cols_to_keep)
+
+df.head()
+df.shape
+
+# rename columns to be more intuitive
+df.rename(columns={
+    'Data_Value': 'pct_overweight',
+    'Race/Ethnicity': 'race',
+    'Age(months)': 'age_months'
+}, inplace=True)
+df.head()
 
 missing_value = df.isna().sum()
 print(missing_value)
@@ -17,10 +30,14 @@ print(missing_value)
 9072/12852 # % missing for age
 10584/12852 # % missing for sex
 
+# dropping rows with missing Data_Value
 df_drop_na = df.dropna(subset=['Data_Value'])
 df_drop_na.head()
 
-df_race = df_drop_na[df_drop_na['Race/Ethnicity'].isin(['American Indian/Alaska Native', 'Non-Hispanic White'])]
+# dropping sex column due to high missingness
+df_drop_sex = df_drop_na.drop(columns='Sex')
+
+df_race = df_drop_sex[df_drop_sex['Race/Ethnicity'].isin(['American Indian/Alaska Native', 'Non-Hispanic White'])]
 df_race.shape
 
 # examining missingness patterns
@@ -50,6 +67,7 @@ df_race.shape
 print(df['Race/Ethnicity'].value_counts())
 print(df['Sex'].value_counts())
 print(df['LocationDesc'].value_counts().sort_values())
+
 
 plt.figure()
 overweight_race = df_race.groupby('Race/Ethnicity')['Data_Value'].mean().reset_index()
